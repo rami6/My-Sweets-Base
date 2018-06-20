4e245430d6d1f5d11643fcc9c41fc3855df0b024
rami6 <30137645+rami6@users.noreply.github.com>
2018-06-20 13:43:44 -0700

    Add models for works app

--- works/admin.py
+++ works/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Work, Recipe
 
-# Register your models here.
+admin.site.register(Work)
+admin.site.register(Recipe)
--- works/models.py
+++ works/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class Work(models.Model):
+    title       = models.CharField(max_length=120)
+    made_date   = models.DateField(default=datetime.date.today, null=True, blank=True)
+    note        = models.CharField(max_length=2000)
+    picture     = models.ImageField(upload_to='work_pic', blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Recipe(models.Model):
+    work_id     = models.ForeignKey('Work', on_delete=models.CASCADE)
+    url         = models.URLField()
+
+    def __str__(self):
+        return self.url
