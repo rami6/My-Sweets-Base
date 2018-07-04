df209658a4c3f8580c46cb4a3e1448df71727f76
rami6 <30137645+rami6@users.noreply.github.com>
2018-07-04 14:29:56 -0700

    Fix models

--- works/models.py
+++ works/models.py
@@ -5,7 +5,7 @@ import datetime
 class Work(models.Model):
     title       = models.CharField(max_length=120)
     made_date   = models.DateField(default=datetime.date.today, null=True, blank=True)
-    note        = models.CharField(max_length=2000)
+    note        = models.TextField(max_length=2000)
     picture     = models.ImageField(upload_to='work_pic', blank=True)
 
     def __str__(self):
@@ -15,6 +15,7 @@ class Work(models.Model):
 class Recipe(models.Model):
     work_id     = models.ForeignKey('Work', on_delete=models.CASCADE)
     url         = models.URLField()
+    link_title  = models.CharField(max_length=120, default="Recipe")
 
     def __str__(self):
         return self.url
