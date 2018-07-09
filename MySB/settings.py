fca12d26132420dcebfd4888615c09637a8ea7f2
rami6 <30137645+rami6@users.noreply.github.com>
2018-07-09 16:44:38 -0700

    Fix error: 415 Unsupported Media Type, when post data

--- MySB/settings.py
+++ MySB/settings.py
@@ -86,10 +86,11 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
         'rest_framework.parsers.FormParser',
         'rest_framework.parsers.MultiPartParser'
      ),
- }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
