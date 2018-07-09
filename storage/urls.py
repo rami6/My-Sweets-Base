5b49bb5ef15bc4b53ea6df58a4237efef77f7b01
rami6 <30137645+rami6@users.noreply.github.com>
2018-07-08 23:27:58 -0700

    Add search view

--- storage/urls.py
+++ storage/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from .views import StorageTopView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', StorageTopView.as_view(), name='top'),
+    path('test/', TemplateView.as_view(template_name='search_test.html')),
 ]
