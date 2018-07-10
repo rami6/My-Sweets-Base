9b6bd74f1162db936ace496aa300beb2089df0da
rami6 <30137645+rami6@users.noreply.github.com>
2018-07-10 14:15:36 -0700

    Add sort feature

--- MySB/apiviews.py
+++ MySB/apiviews.py
@@ -3,8 +3,29 @@ from rest_framework import generics, filters
 from storage.serializers import IngredientSerializer
 
 
-class StorageSearchAPIView(generics.ListAPIView):
+class StorageSearchAPIView_expirationA(generics.ListAPIView):
     queryset = Ingredient.objects.all().order_by('expiration_date', 'name')
+    serializer_class = IngredientSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+
+
+class StorageSearchAPIView_expirationD(generics.ListAPIView):
+    queryset = Ingredient.objects.all().order_by('-expiration_date', 'name')
+    serializer_class = IngredientSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+
+
+class StorageSearchAPIView_nameA(generics.ListAPIView):
+    queryset = Ingredient.objects.all().order_by('name', 'expiration_date')
+    serializer_class = IngredientSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+
+
+class StorageSearchAPIView_nameD(generics.ListAPIView):
+    queryset = Ingredient.objects.all().order_by('-name', 'expiration_date')
     serializer_class = IngredientSerializer
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
--- MySB/urls.py
+++ MySB/urls.py
@@ -4,13 +4,16 @@ from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 from .routers import router
-from .apiviews import StorageSearchAPIView
+from .apiviews import StorageSearchAPIView_expirationA, StorageSearchAPIView_expirationD, StorageSearchAPIView_nameA, StorageSearchAPIView_nameD
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('works/', include('works.urls')),
     path('storage/', include('storage.urls')),
-    path('api/storage/search/', StorageSearchAPIView.as_view()),
+    path('api/storage/e-a/', StorageSearchAPIView_expirationA.as_view()),
+    path('api/storage/e-d/', StorageSearchAPIView_expirationD.as_view()),
+    path('api/storage/n-a/', StorageSearchAPIView_nameA.as_view()),
+    path('api/storage/n-d/', StorageSearchAPIView_nameD.as_view()),
     path('api/', include(router.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
