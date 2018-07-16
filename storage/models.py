fea124d3db1a87c7eec1bee07656b4345d716b67
rami6 <30137645+rami6@users.noreply.github.com>
2018-07-16 16:31:07 -0700

    Make expiration date required

--- storage/models.py
+++ storage/models.py
@@ -4,7 +4,7 @@ from datetime import date
 
 class Ingredient(models.Model):
     name            = models.CharField(max_length=120)
-    expiration_date = models.DateField(null=True, blank=True)
+    expiration_date = models.DateField(default=date.today)
 
     def __str__(self):
         return self.name
