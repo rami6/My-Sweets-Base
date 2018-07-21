fd420b77283c943072a9939d6d2c69f0694162dd
rami6 <30137645+rami6@users.noreply.github.com>
2018-07-21 11:55:58 -0700

    Apply custom template to password reset view

--- MySB/urls.py
+++ MySB/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('settings/', views.edit_account, name='settings'),
     path('settings/success/', TemplateView.as_view(template_name='account_settings_success.html'), name='settings_success'),
     path('change-password/', views.change_password, name='change_password'),
-    path('reset-password/', password_reset, name='reset_password'),
+    path('reset-password/', password_reset, {'template_name': 'reset_password.html'}, name='reset_password'),
     path('reset-password/done/', password_reset_done, name='password_reset_done'),
     url(r'reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm , name='password_reset_confirm'),
     path('reset-password/complete/', password_reset_complete, name='password_reset_complete'),
