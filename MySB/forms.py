fb281f2e9c2d39d2bf04d12ecb847e53c9d7ce2c
rami6 <30137645+rami6@users.noreply.github.com>
2018-08-20 02:12:48 -0700

    Update agreement

--- MySB/forms.py
+++ MySB/forms.py
@@ -4,11 +4,12 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    # if you require user email address, uncomment
+    # email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2', )
+        fields = ('username', 'password1', 'password2',) # if you require user email address, add 'email' inside parentheses
 
 
 class AccountEditForm(UserChangeForm):
