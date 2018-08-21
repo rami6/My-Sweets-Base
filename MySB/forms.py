7d1f6fc7aac42b641e7972db725aded46308170a
rami6 <30137645+rami6@users.noreply.github.com>
2018-08-20 21:19:36 -0700

    Enable to register email address

--- MySB/forms.py
+++ MySB/forms.py
@@ -4,16 +4,15 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    # if you require user email address, uncomment
-    # email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    email = forms.EmailField(max_length=254, help_text='', required=False)
 
     class Meta:
         model = User
-        fields = ('username', 'password1', 'password2',) # if you require user email address, add 'email' inside parentheses
+        fields = ('username', 'password1', 'password2', 'email')
 
 
 class AccountEditForm(UserChangeForm):
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    email = forms.EmailField(max_length=254, help_text='', required=False)
 
     class Meta:
         model = User
